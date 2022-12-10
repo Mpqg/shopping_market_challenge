46cffdcc88b2f582f9df978e05be6edf1ced09f9
Mpqg <mapazquiga@gmail.com>
2022-12-10 15:30:17 -0500

    initial mock up

--- src/App.jsx
+++ src/App.jsx
@@ -17,15 +17,13 @@ function App() {
       <p>Regular Customer <input type="checkbox" /></p>
       <h2 className="text_center">List of products</h2>
       <button>Empty Cart</button>
-      <p>{quantity}</p>
-      <img src="/icons8-carrito-de-compras-30.png" alt="" />
       <table align='center'>
         <thead>
           <tr>
             <th>PHOTO</th>
             <th>ITEM</th>
             <th>PRICE</th>
-            <th></th>
+            <th><img src="/icons8-carrito-de-compras-30.png" alt="" />{quantity}</th>
           </tr>
         </thead>
         <tbody>
@@ -36,9 +34,15 @@ function App() {
             <td><button>add to cart</button></td>
           </tr>
           <tr>
-            <td><img src="/icons8-carrito-de-compras-30.png" alt="" /></td>
-            <td>Milk</td>
-            <td>1.00</td>
+            <td><img src="/soda.png" alt="" width={80} /></td>
+            <td>Red Bull</td>
+            <td>$4.00</td>
+            <td><button>add to cart</button></td>
+          </tr>
+          <tr>
+            <td><img src="/wheat.png" alt="" width={60} /></td>
+            <td>Flour</td>
+            <td>$2.75</td>
             <td><button>add to cart</button></td>
           </tr>
         </tbody>
@@ -57,18 +61,25 @@ function App() {
         <tbody>
           <tr>
             <td>Milk</td>
-            <td><button>+</button> 2 <button>-</button></td>
+            <td><button type='button'>+</button> 2 <button>-</button></td>
             <td>$3.50</td>
             <td>$7.00</td>
           </tr>
           <tr>
             <td>Red Bull</td>
-            <td><button>+</button> 3 <button>-</button></td>
+            <td><button className='add'>+</button> num <button className='less'>-</button></td>
+            <script>
+            let buttonAdd = document.querySelector('.add');
+            let buttonLess = document.querySelector('.less');
+            let num = <document className="getElementById"></document>;
+            
+            </script>
+            
             <td>$4.00</td>
             <td>$12.00</td>
           </tr>
           <tr>
-          <td>Flour</td>
+            <td>Flour</td>
             <td><button>+</button> 1 <button>-</button></td>
             <td>$2.75</td>
             <td>$2.75</td>
@@ -106,7 +117,7 @@ function App() {
             <td>$12.00</td>
           </tr>
           <tr>
-          <td>Flour</td>
+            <td>Flour</td>
             <td>1</td>
             <td>$2.75</td>
             <td>$2.75</td>
@@ -117,13 +128,13 @@ function App() {
       <p>SUB-TOTAL: $17.75</p>
       <p>TAX (6.5%): $1.78</p>
       <p>TOTAL: $19.53</p>
-      <p>CASH: <input type="text" /></p> 
+      <p>CASH: <input type="text" /></p>
       <p>CHANGE: $0.00</p>
       <p>YOU SAVED: $1.75!</p>
       <button>Checkout</button>
       <button>Cancel</button>
-      
-      
+
+
     </div>
   )
 }
