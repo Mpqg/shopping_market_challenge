22407ef499c7d8b570bcfb90f1598d8c854338e2
Mpqg <mapazquiga@gmail.com>
2022-12-10 00:38:22 -0500

    initial mock up

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,48 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cart, addCart] = useState([])
+  const [quantity, setQuantity] = useState(0)
+  const [customerType, setCustomerType] = useState("Regular")
+  const [currentProcess, setCurrentProcess] = useState("Shopping")
+  const [cash, setCash] = useState(0.00)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="text_center"> Jerry's Quick Mart </h1>
+      <h2>Type of client</h2>
+      <p>Rewards Members <input type="checkbox" /></p>
+      <p>Regular Customer <input type="checkbox" /></p>
+      <h2 className="text_center">List of products</h2>
+      <button>Empty Cart</button>
+      <img src="/icons8-carrito-de-compras-30.png" alt="" />
+      <table align='center'>
+        <thead>
+          <tr>
+            <th>Photo</th>
+            <th>Product</th>
+            <th>Price</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td><img src="/icons8-carrito-de-compras-30.png" alt="" /></td>
+            <td>Eggs</td>
+            <td>2.00</td>
+            <td>add to cart</td>
+          </tr>
+          <tr>
+            <td><img src="/icons8-carrito-de-compras-30.png" alt="" /></td>
+            <td>Milk</td>
+            <td>1.00</td>
+            <td>add to cart</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <h2 className="text_center">Shopping Cart</h2>
+      <h2 className="text_center">Checkout</h2>
     </div>
   )
 }
