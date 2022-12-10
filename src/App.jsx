fe51696171c796a657ec238d84cadee692dfcf2f
Mpqg <mapazquiga@gmail.com>
2022-12-10 10:59:01 -0500

    initial mock up

--- src/App.jsx
+++ src/App.jsx
@@ -17,34 +17,113 @@ function App() {
       <p>Regular Customer <input type="checkbox" /></p>
       <h2 className="text_center">List of products</h2>
       <button>Empty Cart</button>
+      <p>{quantity}</p>
       <img src="/icons8-carrito-de-compras-30.png" alt="" />
       <table align='center'>
         <thead>
           <tr>
-            <th>Photo</th>
-            <th>Product</th>
-            <th>Price</th>
-            <th>Action</th>
+            <th>PHOTO</th>
+            <th>ITEM</th>
+            <th>PRICE</th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td><img src="/icons8-carrito-de-compras-30.png" alt="" /></td>
-            <td>Eggs</td>
-            <td>2.00</td>
-            <td>add to cart</td>
+            <td><img src="/icons8-botella-de-leche-100.png" alt="" /></td>
+            <td>Milk</td>
+            <td>$3.50</td>
+            <td><button>add to cart</button></td>
           </tr>
           <tr>
             <td><img src="/icons8-carrito-de-compras-30.png" alt="" /></td>
             <td>Milk</td>
             <td>1.00</td>
-            <td>add to cart</td>
+            <td><button>add to cart</button></td>
           </tr>
         </tbody>
       </table>
 
       <h2 className="text_center">Shopping Cart</h2>
+      <table align='center'>
+        <thead>
+          <tr>
+            <th>ITEM</th>
+            <th>QUANTITY</th>
+            <th>UNIT PRICE</th>
+            <th>TOTAL</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Milk</td>
+            <td><button>+</button> 2 <button>-</button></td>
+            <td>$3.50</td>
+            <td>$7.00</td>
+          </tr>
+          <tr>
+            <td>Red Bull</td>
+            <td><button>+</button> 3 <button>-</button></td>
+            <td>$4.00</td>
+            <td>$12.00</td>
+          </tr>
+          <tr>
+          <td>Flour</td>
+            <td><button>+</button> 1 <button>-</button></td>
+            <td>$2.75</td>
+            <td>$2.75</td>
+          </tr>
+        </tbody>
+      </table>
+      <p>TOTAL NUMBER OF ITEMS SOLD: 6</p>
+      <p>SUB-TOTAL: $17.75</p>
+      <p>TAX (6.5%): $1.78</p>
+      <p>TOTAL: $19.53</p>
+      <button>Proceed to Checkout</button>
+
       <h2 className="text_center">Checkout</h2>
+
+      <table align='center'>
+        <thead>
+          <tr>
+            <th>ITEM</th>
+            <th>QUANTITY</th>
+            <th>UNIT PRICE</th>
+            <th>TOTAL</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Milk</td>
+            <td>2</td>
+            <td>$3.50</td>
+            <td>$7.00</td>
+          </tr>
+          <tr>
+            <td>Red Bull</td>
+            <td>3</td>
+            <td>$4.00</td>
+            <td>$12.00</td>
+          </tr>
+          <tr>
+          <td>Flour</td>
+            <td>1</td>
+            <td>$2.75</td>
+            <td>$2.75</td>
+          </tr>
+        </tbody>
+      </table>
+      <p>TOTAL NUMBER OF ITEMS SOLD: 6</p>
+      <p>SUB-TOTAL: $17.75</p>
+      <p>TAX (6.5%): $1.78</p>
+      <p>TOTAL: $19.53</p>
+      <p>CASH: <input type="text" /></p> 
+      <p>CHANGE: $0.00</p>
+      <p>YOU SAVED: $1.75!</p>
+      <button>Checkout</button>
+      <button>Cancel</button>
+      
+      
     </div>
   )
 }
